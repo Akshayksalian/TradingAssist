252c7897d104456682e777528c617938d9d8d571
Akshay <akshayksalian@gmail.com>
2021-08-19 16:59:48 +0530

    fragments

--- app/src/main/java/com/example/tradingassist/MainActivity.java
+++ app/src/main/java/com/example/tradingassist/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.tradingassist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button logout;
+    BottomNavigationView navigationView;
 
     // Using of fireBase Authentication class.
     FirebaseAuth mAuth;
@@ -21,16 +26,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = findViewById(R.id.logout);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body,new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment=null;
+
+                switch (item.getItemId()){
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case R.id.nav_news:
+                        fragment = new NewsFragment();
+                        break;
+
+                    case R.id.nav_socialmedia:
+                        fragment = new SocialFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body,fragment).commit();
+
+                return true;
+            }
+        });
 
         // Initializing fire base auth into onCreate method.
         mAuth=FirebaseAuth.getInstance();
-
-        // clicklistener for logout button.
-        logout.setOnClickListener(view ->{
-            mAuth.signOut();
-            startActivity(new Intent(MainActivity.this,Login.class));
-        });
     }
 
     // Overriding on start method.
