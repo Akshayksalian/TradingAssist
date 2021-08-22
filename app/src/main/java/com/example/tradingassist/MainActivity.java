31e89842d0bffd196965bd8c6671cf35d7591809
Akshay <akshayksalian@gmail.com>
2021-08-22 19:50:39 +0530

    Recycler view

--- app/src/main/java/com/example/tradingassist/MainActivity.java
+++ app/src/main/java/com/example/tradingassist/MainActivity.java
@@ -3,13 +3,10 @@ package com.example.tradingassist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
-import android.widget.Button;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
